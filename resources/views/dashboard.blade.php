192f8159e69316e7632a98899bf985e7a0cd9d81
Vitor Ramos <vitorramosti@gmail.com>
2023-08-29 17:03:26 -0300

    asddsda

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,6 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-
             <form action="{{route('question.store')}}" method="post">
                 @csrf
                 <div class="mb-4">
